501032e348fc8343490de51f3cd4d399f443f8a1
Filip Tijan <ftijan@gmail.com>
2021-10-09 14:56:23 +0100

    adding storybook knobs, usage

--- src/src/app/components/link/stories/link.stories.ts
+++ src/src/app/components/link/stories/link.stories.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { moduleMetadata } from '@storybook/angular';
 import { Story } from '@storybook/angular/types-6-0';
 import { LinkComponent } from '../link.component';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 
 // This exports the Stories group for this component
 export default {
@@ -18,6 +19,8 @@ export default {
       declarations: [LinkComponent],
       imports: [CommonModule],
     }),
+    // we add the withKnobs decorator in order to use it
+    withKnobs,
   ],
 };
 
@@ -25,8 +28,12 @@ export default {
 const Template: Story<LinkComponent> = () => ({
   component: LinkComponent,
   props: {
-    color: 'primary',
-    content: 'Visit Storybook',
+    // we use the select knob to set the color value
+    color: select('Color', ['primary', 'secondary'] , 'primary'),
+    
+    // we use the text knob to set the ngContent value
+    content: text('Text', 'Visit Storybooks'),
+    
     href: 'https://storybook.js.org/',
     target: '_blank',
   },
